3aca50386926f7335b53a0852f972da33bffe699
Yizhou <i@yizhou.ac.cn>
2026-04-11 14:06:30 +0800

    fix: scan-deps and stop-scache

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -3,9 +3,11 @@ cmake_minimum_required(VERSION 3.30)
 set(CMAKE_C_COMPILER clang CACHE STRING "")
 set(CMAKE_CXX_COMPILER clang++ CACHE STRING "")
 
-find_program(CLANG_SCAN_DEPS_PATH "clang-scan-deps")
+find_program(CLANG_SCAN_DEPS_PATH NAMES "clang-scan-deps" NO_CACHE)
 if(CLANG_SCAN_DEPS_PATH)
     set(CMAKE_CXX_COMPILER_CLANG_SCAN_DEPS "${CLANG_SCAN_DEPS_PATH}" CACHE FILEPATH "")
+else()
+    unset(CMAKE_CXX_COMPILER_CLANG_SCAN_DEPS CACHE)
 endif()
 
 find_program(LLVM_AR_PATH "llvm-ar")
